7c9dfca4068300a4bdcacb22098e76db2891cc1a
TamirYaariTAU <tamiryaari@mail.tau.ac.il>
2022-03-18 17:26:03 +0200

    Cleaner code

--- client/data.js
+++ client/data.js
@@ -1,65 +1,22 @@
-// Get the button and container from HTML:
-const button = document.getElementById("theButton")
+const postToPython = document.getElementById("postToPython")
 const before = document.getElementById("before")
 const after = document.getElementById("after")
+const inputSubmit = document.getElementById("inputSubmit");
 
-// make x and y as input from user
-
-// insert all the logic that happens after submit to the submit function - using additional functions
-// need to check how it is done without just disappearing immediately :(
-
-
-// const form = document.getElementById("form");
-// const log = document.getElementById("log");
-const submit = document.getElementById("submit");
-
-function logSubmit(event) {
+inputSubmit.addEventListener('click',function () {
     x = document.getElementById("x").value;
     y = document.getElementById("y").value;
-    // x = x_inp;
-    // y = y_inp;
     before.innerHTML += "Sending to Python:" + "<p>" +
         "x = " + x + "<p>" + "y = " + y + "<p>";
-    // event.preventDefault();
-    console.log("in first button click func : x = "+x);
-}
+});
 
-// form.addEventListener('submit',logSubmit);
+let x; // this can be the picture
+let y; // this can be the level of privacy-usability or whatever
 
-submit.addEventListener('click',logSubmit);
-
-let x = 1; // this can be the picture
-let y = 3; // this can be the level of privacy-usability or whatever
-
-console.log("out of func : x = "+x);
-
-// submit.onclick = function () {
-//     let x_inp = document.getElementById("x").value;
-//     let y_inp = document.getElementById("y").value;
-//     // log.textContent = x_inp + "<pr>" + y_inp;
-//     log.innerHTML = x_inp + "<br>" + y_inp;
-//     x = x_inp;
-//     y = y_inp;
-//     before.innerHTML += "Sending to Python:" + "<p>" +
-//         "x = " + x + "<p>" + "y = " + y + "<p>";
-// }
-
-let inputs = [x, y];
-// let inputs_json = [
-//     {"x": x, "y": y}
-// ];
-
-// before.innerHTML += "Sending to Python:" + "<p>" +
-//     "x = " + x + "<p>" + "y = " + y + "<p>";
-
-// Create an event listener on the button element:
-button.onclick = function () {
+postToPython.onclick = function () {
     let inputs_json = [
         {"x": parseInt(x), "y": parseInt(y)}
     ];
-
-    console.log("in post to python func : x = "+x);
-
 
     // Get the receiver endpoint from Python using fetch:
     fetch("http://127.0.0.1:5000/receiver",
