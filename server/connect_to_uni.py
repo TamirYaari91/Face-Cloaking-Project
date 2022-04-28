e9efe45d36a6ae3432e47500ec1df337ec3d004f
TamirYaariTAU <tamiryaari@mail.tau.ac.il>
2022-04-28 22:24:15 +0300

    client connects to c-008 server, runs face-off with given parameters

--- server/connect_to_uni.py
+++ server/connect_to_uni.py
@@ -1,28 +1,74 @@
+import os
+import random
+
 import paramiko
 
-ls = "ls -l"
+ssh_port = 22
+my_username = "tamiryaari"
+my_password = ""  # TODO - insert password here
+nova = "nova.cs.tau.ac.il"
+c_008 = "c-008.cs.tau.ac.il"
+filename_for_perturbated_image = "/pert.jpg"
+
+margin = str(6)
+amplification = str(3)
+celebs = ['barack', 'bill', 'jenn', 'leo', 'mark', 'matt', 'melania', 'meryl', 'morgan', 'taylor']
+commands = \
+    [
+        "conda activate tf-gpu",
+        "cd /home/sharifm/teaching/uspw-0368-3544/2022-spring/group-04/face-off",
+        "python src/attack.py --source myface --pair-flag true --margin " + margin + " --amplification " + amplification
+    ]
+
+full_command = "\n".join(commands)
 
 
-def connect_to_nova(command):
-    host = "nova.cs.tau.ac.il"
-    port = 22
-    username = "tamiryaari"
-    password = ""  # TODO - insert password here
+def get_path_to_final_perturbation(lines):
+    last_pert_ind = -1
+    pic_ind = 0
+    for i in range(len(lines) - 1, -1, -1):
+        line_start = lines[i][0:8]
+        if line_start == "SUCCESS!":
+            last_pert_ind = i
+            pic_ind += 1
+            if pic_ind == 2:
+                break
+    return lines[last_pert_ind].split("to ")[-1][:-1]
 
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(host, port, username, password)
 
-    stdin, stdout, stderr = ssh.exec_command(command)
-    lines = stdout.readlines()
-    if ssh is not None:
-        ssh.close()
-        del ssh, stdin, stdout, stderr
+def download_file_from_path(ssh_client, remote_file_path, local_file_path):
+    ftp_client = ssh_client.open_sftp()
+    ftp_client.get(remote_file_path, local_file_path)
+    ftp_client.close()
+
+
+face_off_ret_val = ['Loading Images...\n',
+                    'SUCCESS! Images written to /home/sharifm/teaching/uspw-0368-3544/2022-spring/group-04/face-off/data/new_adv_imgs/cw_l2/small_center/hinge_loss/full/cw_l2_small_center_h_loss_face_pic_bill_marg_5.40_amp_2.500.png\n',
+                    'SUCCESS! Images written to /home/sharifm/teaching/uspw-0368-3544/2022-spring/group-04/face-off/data/new_adv_imgs/cw_l2/small_center/hinge_loss/crop/cw_l2_small_center_h_loss_face_pic_bill_marg_5.40_amp_2.500.png\n',
+                    'Amplifying and Writing Images----------------------5.195116334129125\n',
+                    'Saving Numpy Array---------------------------------0.01307652611285448\n',
+                    'total running time =  165.0762403011322\n']
+
+
+def connect_to_host(host, username, password, port, command):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(host, port, username, password)
+
+    # Perform Face-Off
+    # stdin, stdout, stderr = ssh.exec_command(command)
+    # lines = stdout.readlines()
+    lines = face_off_ret_val
+
+    final_perturbation_path = get_path_to_final_perturbation(lines)
+    download_file_from_path(ssh_client, final_perturbation_path, os.getcwd() + filename_for_perturbated_image)
+
+    if ssh_client is not None:
+        ssh_client.close()
+        # del ssh, stdin, stdout, stderr
     return lines
 
 
-res = connect_to_nova(ls)
-for i in range(len(res)):
-    line = res[i].split(" ")
-    if i > 0:
-        print(line[-1])
+res = connect_to_host(c_008, my_username, my_password, ssh_port, full_command)
+
+# print(random.choice(celebs))
