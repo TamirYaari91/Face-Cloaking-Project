Fixing gradient error

Ulixes/PGD.py
@@ -1,11 +1,10 @@
-import math
 import torch
 import torch.nn as nn
 import numpy as np
 from facenet_pytorch.models.mtcnn import MTCNN
 from facenet_pytorch.models.inception_resnet_v1 import InceptionResnetV1
 from PIL import Image
-from torch import autograd
+from torch.autograd import grad
 
 EPSILON = 0.0001
 EMBEDDING_MODEL = InceptionResnetV1(pretrained="vggface2").eval()
@@ -15,16 +14,19 @@ filename_for_perturbated_image_ulixes = "cloaked_cropped_ulixes.jpg" # TODO - ne
 
 def Ulixes(image, margin):
     cropped_image = crop_image(image, filename_for_original_image_cropped)
-    cloaked_image_tensor = pgd(cropped_image, margin)
-    cloaked_image_array_normalized = normalize_cloaked_image_tensor(cloaked_image_tensor).detach().numpy()
+    noise_mask = pgd(cropped_image, margin) # PGD
 
-    cloaked_image_array_normalized = np.swapaxes(cloaked_image_array_normalized, 0, 1)  # [3, 160, 160] -> [160, 3, 160]
-    cloaked_image_array_normalized = np.swapaxes(cloaked_image_array_normalized, 1, 2)  # [160, 3, 160] -> [160, 160, 3]
-
-    cloaked_image = Image.fromarray(cloaked_image_array_normalized.astype(np.uint8))
-    # cloaked_image.save("cloaked_image.png")
-    cloaked_image.save(filename_for_perturbated_image_ulixes)
-    return cloaked_image
+    # TODO: un_comment those lines and understand how to take the (1,512) dimensions noise and turn it into (160, 160, 3) dimensions matrix
+    # cropped_image_with_mask = add_noise_mask(cropped_image, noise_mask)
+    # cloaked_image_array_normalized = normalize_cloaked_image_tensor(cropped_image_with_mask)
+    #
+    # cloaked_image_array_normalized = np.swapaxes(cloaked_image_array_normalized, 0, 1)  # [3, 160, 160] -> [160, 3, 160]
+    # cloaked_image_array_normalized = np.swapaxes(cloaked_image_array_normalized, 1, 2)  # [160, 3, 160] -> [160, 160, 3]
+    #
+    # cloaked_image = Image.fromarray(np.array(cloaked_image_array_normalized).astype(np.uint8))
+    # cloaked_image.save(filename_for_perturbated_image_ulixes)
+    # return cloaked_image
+    return
 
 
 def crop_image(image, cropped_path):
@@ -36,38 +38,48 @@ def crop_image(image, cropped_path):
 
 def pgd(image, margin=1.6, alpha=0.01):
     # margin: set to 1.1 as default, can be between [0.2, 2] to change intensity of noise masks introduced
-    anchor = positive = image
-    negative = add_epsilon_noise(image)
-    negative.requires_grad_()
-    positive.requires_grad_()
-    embedded_negative = get_embedding(negative)
-    embedded_positive = get_embedding(positive)
-    distance_positive_negative = np.linalg.norm(embedded_positive.detach() - embedded_negative.detach())
-    # triplet_loss = nn.TripletMarginLoss(margin=margin, p=2)
-    threshold = 0.001
-    count = 0
 
-    while True:
-        # print(count)
-        anchor.requires_grad_()
-        embedded_anchor = get_embedding(anchor)
-        loss_number = get_loss(embedded_anchor, embedded_positive, distance_positive_negative, margin)
-        loss = torch.as_tensor(loss_number)
-        loss.requires_grad_()
-        # print(f"loss is: {loss_number}")
-        g = autograd.grad(loss, loss)
-        prev_anchor = anchor
-        anchor = torch.add(anchor, scale(g, alpha))
-        difference_of_prev_anchor_and_new_anchor = np.linalg.norm(
-            get_embedding(anchor).detach() - get_embedding(prev_anchor).detach())
-        # print(f"new noise is: {difference_of_prev_anchor_and_new_anchor}")
-        # if math.copysign(1, np.linalg.norm(loss.detach().numpy(), np.inf)) != 1 or difference_of_prev_anchor_and_new_anchor < threshold:
-        if difference_of_prev_anchor_and_new_anchor < threshold or count == 100:
+    anchor = image
+    positive = image
+    negative = add_epsilon_noise(image)
+
+    adv_triplet_margin_loss = nn.TripletMarginLoss(margin)
+
+    embedded_anchor = torch.autograd.Variable(get_embedding(anchor))
+    embedded_negative = torch.autograd.Variable(get_embedding(negative), requires_grad=True)
+    embedded_positive = torch.autograd.Variable(get_embedding(positive))
+
+    threshold = 0.01
+    epochs = 100
+
+    noise_mask = 0
+
+    for i in range(epochs):
+
+        loss = adv_triplet_margin_loss(embedded_positive, embedded_anchor, embedded_negative)
+        loss.backward()
+        gradient = (- 1) * embedded_negative.grad  # After checking the loss gradient calculation I think we need this
+
+        new_noise = scale(gradient[0], alpha)
+        noise_mask += new_noise  # obtaining total noise added
+
+        embedded_negative.grad.zero_()
+
+        anchor = torch.add(embedded_anchor, new_noise)
+
+        difference_of_prev_anchor_and_new_anchor = np.linalg.norm(anchor.data - embedded_anchor.data)
+        if difference_of_prev_anchor_and_new_anchor < threshold:
             break
-        anchor = np.clip(anchor.detach(), positive.detach() - 0.5, positive.detach() + 0.5)
-        anchor = np.clip(anchor.detach(), -1, 1)
-        count += 1
-    return anchor
+
+        anchor = np.clip(anchor.detach(), embedded_positive.detach() - 0.5, embedded_positive.detach() + 0.5)
+        anchor = np.clip(anchor, -1, 1)
+
+        embedded_anchor = anchor
+
+        print(f"Epoch {i}/{epochs}: Loss: {loss}")
+
+    anchor = torch.add(embedded_anchor, noise_mask)
+    return noise_mask
 
 
 def get_loss(anchor, positive, const_distance_positive_negative, margin):
@@ -80,14 +92,18 @@ def add_epsilon_noise(image):
     return image
 
 
+def add_noise_mask(image, noise):
+    image = torch.add(image, noise)
+    return image
+
+
 def get_embedding(image):
     return EMBEDDING_MODEL(image.unsqueeze(0))
 
 
 def scale(vector, alpha):
     inf_norm = np.linalg.norm(vector, np.inf)
-    # print(vector[0].item() / inf_norm)
-    return alpha * (vector[0].detach() / inf_norm)
+    return (vector.data / inf_norm).data * alpha
 
 
 def normalize_cloaked_image_tensor(image):
@@ -99,4 +115,4 @@ def normalize_cloaked_image_tensor(image):
 
 
 if __name__ == '__main__':
-    Ulixes("C:\matt.jpg", 0.8)
+    Ulixes("C:\matt.jpg", 1.1)